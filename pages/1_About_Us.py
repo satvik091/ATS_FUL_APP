225feb26584418eca2794fd62d40255ae6e57138
SATVIK GUPTA <satvikg21111124@gmail.com>
2025-02-03 12:36:55 +0530

    Update 1_About_Us.py

--- pages/1_About_Us.py
+++ pages/1_About_Us.py
@@ -5,24 +5,26 @@ def show():
     check_authentication()
     
     # Streamlit UI
+    st.image("https://seeklogo.com/images/J/Jobfit-logo-B6E8FE9C02-seeklogo.com.gif", width=150) 
     st.markdown("<h1 style='text-decoration: underline;'>JobFit AI – Smart Hiring Made Easy</h1>", unsafe_allow_html=True)
-    st.write("🚀 **AI-Powered Resume Matching**")
 
+    
     # Features
-    st.write("Uses Google Gemini API for advanced NLP & machine learning.")
-    st.write("Analyzes resumes based on skills, experience, and job relevance.")
+    st.write("🚀 **AI-Powered Resume Matching**")
+    st.write("👉Uses Google Gemini API for advanced NLP & machine learning.")
+    st.write("👉Analyzes resumes based on skills, experience, and job relevance.")
 
     st.write("📊 **Automated Ranking System**")
-    st.write("Scores and ranks candidates based on suitability.")
-    st.write("Helps recruiters identify top talent instantly.")
+    st.write("👉Scores and ranks candidates based on suitability.")
+    st.write("👉Helps recruiters identify top talent instantly.")
 
     st.write("⚡ **Seamless & Efficient Hiring**")
-    st.write("Streamlit-powered UI for an interactive experience.")
-    st.write("Reduces manual screening and shortens hiring time.")
+    st.write("👉Streamlit-powered UI for an interactive experience.")
+    st.write("👉Reduces manual screening and shortens hiring time.")
 
     st.write("📂 **ATS Integration & Data-Driven Insights**")
-    st.write("Easily integrates with Applicant Tracking Systems (ATS).")
-    st.write("Provides real-time analytics for better hiring decisions.")
+    st.write("👉Easily integrates with Applicant Tracking Systems (ATS).")
+    st.write("👉Provides real-time analytics for better hiring decisions.")
 
 
 if __name__ == "__main__":
