7fd07edf98730213e0e278b013896b82d79e7062
SATVIK GUPTA <satvikg21111124@gmail.com>
2025-10-16 12:37:57 +0530

    Update 4_Resume_Ranker.py

--- pages/4_Resume_Ranker.py
+++ pages/4_Resume_Ranker.py
@@ -8,7 +8,7 @@ import pandas as pd
 from auth import check_authentication
 
 # Configuration for Gemini API
-GOOGLE_API_KEY = ("AIzaSyDJNmx7PKmb92aHcrwBK7L5IKHipNzjVck")
+GOOGLE_API_KEY = ("AIzaSyC8bVdgvOD9r5kBDchtrX9sUJbQqgPBebc")
 if not GOOGLE_API_KEY:
     st.error("Please set GOOGLE_API_KEY in Streamlit secrets")
     st.stop()
